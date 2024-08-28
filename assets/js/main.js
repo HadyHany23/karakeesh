d337140aa9b6510dbba8721251c43fa7683b2156
Hady Hany <hadyharb1988@gmail.com>
2024-08-28 17:21:39 +0300

    final before breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,34 @@ const blurHeader = () => {
 };
 window.addEventListener("scroll", blurHeader);
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  //when the scroll is greater than 350 viewport height,add the scroll-header to the header tag
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
